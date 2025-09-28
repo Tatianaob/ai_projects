712280a79e4d54f5eb72e51a5b1003206ac6ed33
Tatianaob <ltatiobandore@gmail.com>
2025-09-27 18:12:32 -0700

    Add matrix

--- src/day4.py
+++ src/day4.py
@@ -1,5 +1,4 @@
 # Spam mail detector. Using Scikit-Learn
-
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
@@ -25,4 +24,19 @@ print(X_test)
 
 # Evaluate the model using accuracy, confusion matrix, precision, recall and F1 score
 accuracy = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred)
+recall = recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
+
+print(f"Accuracy: {accuracy}")
+print(f"Presicion: {precision}")
+print(f"Recall: {recall}")
+print(f"F1 score: {f1}")
+
+# Visualize the confusion matrix using Seaborns heatmap
+
+cm = confusion_matrix(y_test, y_pred)
+sns.heatmap(cm, annot=True, fmt='d')
+plt.title('Confusion Matrix')
+plt.show()
 
